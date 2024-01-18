67e4608e355736b9e1be2c11f7ffd2b6ff0688f0
Juan Anzola <jsanzolac@gmail.com>
2024-01-18 04:22:17 -0500

    Congifure repository and add blocks definitions

--- squareRoot/src/main/scala/Main.scala
+++ squareRoot/src/main/scala/Main.scala
@@ -2,19 +2,23 @@ object Main extends App {
 
   def abs(x:Double) = if (x < 0) -x else x
 
-  def sqrIter(guess: Double, x: Double): Double = 
-    if (isGoodEnough(guess, x)) guess
-    else sqrIter(improve(guess, x), x)
+  def sqrt(x:Double) = {
 
-  def isGoodEnough(guess: Double, x: Double) =
-    abs(guess * guess - x) / x < 0.001
+    def sqrIter(guess: Double, x: Double): Double = 
+      if (isGoodEnough(guess, x)) guess
+      else sqrIter(improve(guess, x), x)
 
-  def improve(guess: Double, x:Double) = 
-   (guess + x / guess ) / 2 
+    def isGoodEnough(guess: Double, x: Double) =
+      abs(guess * guess - x) / x < 0.001
 
-  def sqrtRoot(x:Double) = sqrIter(1.0, x)
+    def improve(guess: Double, x:Double) = 
+    (guess + x / guess ) / 2 
 
-  var result = sqrtRoot(4)
+    sqrIter(1.0, x)
+
+  }
+
+  var result = sqrt(4)
 
   println( result )
 }
